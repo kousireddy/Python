42b46cb010f2622a0b317c71cf770566fd34ea77
kousireddy <kousalyavundela2410@gmail.com>
2025-06-18 21:55:58 +0530

    day5python

--- main.py
+++ main.py
@@ -1,28 +1,4 @@
-<<<<<<< HEAD
-#String maipulation
 
-name = input("Enter your name: ")
-print(name)                                      #print stirng
-print(name[2:])      
-print(name[3:6])                            #slicing
-print("My name is :"+name)                       #concatination
-print(len(name))
-
-
-# case conversion
-
-print(name.upper()) #upper
-print(name.lower()) #lower
-print(name.capitalize())  #capitalize first character
-
-y=input("Enter a sentence")
-print(y.title())       #capitalizes each letter in a word in sentence
-
-s = "i am from india"
-print("india" in s)  #returns boolean true or false
-
-
-=======
 import store.arraysquare as arrsq
 import store.addition
 arr1=[]
@@ -59,4 +35,3 @@ data={
 df=pd.DataFrame(data,index=["ece","ece","ece","ece"])
 print(df)
 
->>>>>>> 073523b (day5python)
