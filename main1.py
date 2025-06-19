a37cb39428334b87b0889420a74eb71d2126574f
kousireddy <kousalyavundela2410@gmail.com>
2025-06-19 21:21:30 +0530

    day1python

---
+++ main1.py
@@ -0,0 +1,23 @@
+#String maipulation
+
+name = input("Enter your name: ")
+print(name)                                      #print stirng
+print(name[2:])      
+print(name[3:6])                            #slicing
+print("My name is :"+name)                       #concatination
+print(len(name))
+
+
+# case conversion
+
+print(name.upper()) #upper
+print(name.lower()) #lower
+print(name.capitalize())  #capitalize first character
+
+y=input("Enter a sentence")
+print(y.title())       #capitalizes each letter in a word in sentence
+
+s = "i am from india"
+print("india" in s)  #returns boolean true or false
+
+
